2ea6d4388dbc0956875672f7212aa8b2d9f9ec9d
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-18 11:04:42 +0900

    jsp

---
+++ src/main/java/com/jsp/chap04/DeleteServlet.java
@@ -0,0 +1,17 @@
+package com.jsp.chap04;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/dancer/delete")
+public class DeleteServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 클라이언트에서 전송한 이름을 읽어야함
+    }
+}
